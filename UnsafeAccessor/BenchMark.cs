0758eb5e2a869c2e9e84b74ca4ac9e635c69e6f5
Sharmila Subbiah <sharmila.subbiah@youlend.com>
2024-01-23 09:06:45 +0000

    Add project files.

---
+++ UnsafeAccessor/BenchMark.cs
@@ -0,0 +1,64 @@
+﻿
+namespace UnsafeAccessor
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class BenchMark
+    {
+        [Benchmark]
+        public void UnsafeMethodReflection()
+        {
+            var unsafeExample = new UnsafeExample();
+
+            var result = typeof(UnsafeExample).GetMethod("UnsafeMethodReflection", BindingFlags.Instance | BindingFlags.NonPublic)?
+                .Invoke(unsafeExample, Array.Empty<object>());
+
+        }
+
+        [Benchmark]
+        public void UnsafeMethodAccessor()
+        {
+            var unsafeAccessorExample = new UnsafeExample();
+
+            var result = UnsafeAccessorClass.GetUnsafeName(unsafeAccessorExample);
+        }
+
+        [Benchmark]
+        public void SafeMethodAccessor()
+        {
+            var unsafeAccessorExample = new SafeExample();
+
+            var result = unsafeAccessorExample.Name;
+        }
+    }
+
+    public class UnsafeAccessorClass
+    {
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "UnsafeMethod")]
+        public static extern string GetUnsafeName(UnsafeExample unsafeExample);
+    }
+
+    public class UnsafeExample
+    {
+        private string Name = "Unsafe Name";
+        private string UnsafeMethod()
+        {
+            return "Unsafe Name";
+        }
+    }
+
+    public class SafeExample
+    {
+        public string Name = "Unsafe Name";
+        public string UnsafeMethod()
+        {
+            return "Unsafe Name";
+        }
+    }
+}
---
+++ UnsafeAccessor/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Running;
+using UnsafeAccessor;
+
+BenchmarkRunner.Run<BenchMark>();
+
+var unsafeExample = new UnsafeAccessorExample();
+
+var unsafeAccessorMethod = UnsafeAccessorClass.GetUnsafeStaticField(unsafeExample);
+
+UnsafeAccessorClass.GetUnsafeStaticField(unsafeExample) = Guid.NewGuid();
+Console.WriteLine(unsafeAccessorMethod);
+
+public class UnsafeAccessorClass
+{
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "UnsafeMethod")]
+    public static extern string GetUnsafeMethod(UnsafeAccessorExample unsafeExample);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "Id")]
+    public static extern ref Guid GetUnsafeField(UnsafeAccessorExample unsafeExample);
+
+    [UnsafeAccessor(UnsafeAccessorKind.StaticField, Name = "StaticId")]
+    public static extern ref Guid GetUnsafeStaticField(UnsafeAccessorExample unsafeExample);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "get_Number")]
+    public static extern int GetUnsafeProperty(UnsafeAccessorExample unsafeExample);
+
+}
+
+public class UnsafeAccessorExample
+{
+    
+
+    private static readonly Guid StaticId = Guid.Empty;
+
+    private Guid Id = Guid.Empty;
+    private string UnsafeMethod()
+    {
+        return "Unsafe Name";
+    }
+}
